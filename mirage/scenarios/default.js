8436771aa47129008d00e8b925dfc8b0a2e88502
Tarik Eminagic <tarik@east.ba>
2018-02-02 14:57:13 +0100

    smarter seeding :D

--- mirage/scenarios/default.js
+++ mirage/scenarios/default.js
@@ -1,11 +1,7 @@
 export default function(server) {
-  let users = server.createList('user', 5);
+  let users = server.createList('user', Math.floor(Math.random() * 5) + 1);
 
-  users.forEach(user => {
-    let userBlogs = server.createList('blog', Math.floor(Math.random() * 5));
-
-    userBlogs.forEach(blog => {
-      blog.update('author', user);
-    });
+  users.forEach(author => {
+    server.createList('blog', Math.floor(Math.random() * 6) + 1, { author });
   });
 }
